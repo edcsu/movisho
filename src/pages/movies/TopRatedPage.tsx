0b10fa391ea9ae4889137b3c8aecf8328bc94e3c
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-04-09 01:34:04 +0300

    feat: integrate movie API for other movie pages

--- src/pages/movies/TopRatedPage.tsx
+++ src/pages/movies/TopRatedPage.tsx
@@ -1,7 +1,41 @@
+import { Suspense } from "react";
+import { useLoaderData, Await } from "react-router-dom";
+import MovieList from "../../components/UI/MovieList";
+
 const TopRatedPage = () => {
+  const { results } = useLoaderData();
   return (
-    <>TopRatedPage</>
+    <Suspense fallback={<p style={{textAlign: 'center'}}>Loading....</p>}>
+      <Await resolve={results}>
+        {(foundResults) => <MovieList response={foundResults} />}
+      </Await>
+    </Suspense>
   )
 }
 
 export default TopRatedPage
+
+const loadMovieResults = async() => {
+  const response = await fetch(`${import.meta.env.VITE_TMDB_BASE_URL}/top_rated?language=en-US&page=1`,{
+    headers: {
+      'Content-Type' : 'application/json',
+      'Authorization' : `Bearer ${import.meta.env.VITE_BEARER_TOKEN}`
+    },
+  });
+    if (!response.ok) {
+      const body = { message: 'Could not fetch movies' }
+      const myOptions = { status: 500, statusText: "Something failed" };
+      // throw new Response(JSON.stringify(body), myOptions)
+      return Response.json(body, myOptions)
+      
+    } else {
+      const resData = await response.json()
+      return resData
+    }
+}
+
+export const loader = async () => {
+  return {
+    results : loadMovieResults()
+  }
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router";
 import MovieDetails from "../pages/movies/MovieDetailsPage";
 import RootLayout from "../layout/RootLayout";
-import MoviesPage, { loader as MoviesNowLoader } from "../pages/movies/IndexPage";
+import MoviesPage, { loader as moviesNowLoader } from "../pages/movies/IndexPage";
 import Search from "../pages/movies/SearchPage";
 import ErrorPage from "../pages/ErrorPage";
-import TopRatedPage from "../pages/movies/TopRatedPage";
-import PopularPage from "../pages/movies/PopularPage";
-import UpcomingPage from "../pages/movies/Upcoming";
+import TopRatedPage, { loader as topRatedLoader } from "../pages/movies/TopRatedPage";
+import PopularPage, { loader as popularMoviesLoader } from "../pages/movies/PopularPage";
+import UpcomingPage, { loader as upcomingLoader } from "../pages/movies/Upcoming";
 
 const router = createBrowserRouter([
     {
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
             {
                 index : true,
                 element : <MoviesPage />,
-                loader: MoviesNowLoader
+                loader: moviesNowLoader
             },
             {
                 path : 'movies',
@@ -29,15 +29,18 @@ const router = createBrowserRouter([
                     },
                     {
                         path: 'popular',
-                        element : <PopularPage />
+                        element : <PopularPage />,
+                        loader: popularMoviesLoader
                     },
                     {
                         path: 'top',
-                        element : <TopRatedPage />
+                        element : <TopRatedPage />,
+                        loader: topRatedLoader
                     },
                     {
                         path: 'upcoming',
-                        element : <UpcomingPage />
+                        element : <UpcomingPage />,
+                        loader: upcomingLoader
                     },
                     {
                         path:  ':id',
