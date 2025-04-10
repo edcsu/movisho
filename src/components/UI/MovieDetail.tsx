ec9aefac0ed8e00443dee8a8d3785dc5a4c8d5f3
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-04-10 09:19:00 +0300

    style: add more movie details

--- src/components/UI/MovieDetail.tsx
+++ src/components/UI/MovieDetail.tsx
@@ -1,4 +1,7 @@
 import MovieDetailResponse from "../../types/moviedetail";
+import FamilyLogo from "../../assets/family.svg"
+import EighteenLogo from "../../assets/rated.svg"
+import { formatPriceToDollar } from "../../utils/converters";
 
 type Props = {
   movie: MovieDetailResponse
@@ -6,6 +9,14 @@ type Props = {
 
 const MovieDetail: React.FC<Props> = ({ movie } : Props) => {
   const moviePic = `${import.meta.env.VITE_TMDB_IMAGE_URL}/${movie.poster_path}`
+  const movieDate = new Date(movie.release_date).toLocaleDateString('en-us', {
+    day : 'numeric',
+    month : 'long',
+    year : 'numeric'
+  })
+  const movieBudget = formatPriceToDollar(movie.budget)
+  const movieRevenue = formatPriceToDollar(movie.revenue)
+
   return (
     <section>
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
@@ -40,6 +51,20 @@ const MovieDetail: React.FC<Props> = ({ movie } : Props) => {
                 className="-my-3 divide-y divide-gray-200 text-sm *:even:bg-gray-50 dark:divide-gray-700 dark:*:even:bg-gray-800"
               >
                 <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                  <dt className="font-medium text-gray-900 dark:text-white">Rating</dt>
+
+                  <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">
+                    {movie.vote_average}/10 ({movie.vote_count} votes)
+                  </dd>
+                </div>
+
+                <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                  <dt className="font-medium text-gray-900 dark:text-white">Release date</dt>
+
+                  <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">{movieDate}</dd>
+                </div>
+                
+                <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
                   <dt className="font-medium text-gray-900 dark:text-white">Runtime</dt>
 
                   <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">{movie.runtime} minutes</dd>
@@ -48,19 +73,19 @@ const MovieDetail: React.FC<Props> = ({ movie } : Props) => {
                 <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
                   <dt className="font-medium text-gray-900 dark:text-white">Budget</dt>
 
-                  <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">{movie.budget}</dd>
+                  <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">{movieBudget}</dd>
                 </div>
 
                 <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
                   <dt className="font-medium text-gray-900 dark:text-white">Revenue</dt>
 
-                  <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">{movie.revenue}</dd>
+                  <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">{movieRevenue}</dd>
                 </div>
 
                 <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
-                  <dt className="font-medium text-gray-900 dark:text-white">Release date</dt>
+                  <dt className="font-medium text-gray-900 dark:text-white">Tagline</dt>
 
-                  <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">{movie.release_date}</dd>
+                  <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">{movie.tagline}</dd>
                 </div>
 
                 <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
@@ -68,6 +93,16 @@ const MovieDetail: React.FC<Props> = ({ movie } : Props) => {
 
                   <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">
                     {movie.imdb_id}
+                  </dd>
+                </div>
+
+                <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                  <dt className="font-medium text-gray-900 dark:text-white">Movie audience</dt>
+
+                  <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">
+                    {movie.adult ? 
+                    (<img src={EighteenLogo} alt="rated movie" />) :
+                    (<img src={FamilyLogo} alt="not rated movie" width={150} />)}
                   </dd>
                 </div>
               </dl>
