93a41e1a5d1b1888111c13f3ef1cf470e2012f77
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-04-10 15:24:11 +0300

    style: add pagination component

---
+++ src/components/UI/AppPagination.tsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router"
+
+const AppPagination: React.FC = () => {
+  return (
+    <div className="flex justify-center mb-6">
+      <NavLink to="/" className="flex items-center px-4 py-2 mx-1 text-gray-500 bg-white rounded-md cursor-not-allowed dark:bg-gray-800 dark:text-gray-600">
+          previous
+      </NavLink>
+
+      <NavLink to="/" className="items-center hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:flex dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-600 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+          1
+      </NavLink>
+
+      <NavLink to="/" className="items-center hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:flex dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-600 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+          2
+      </NavLink>
+
+      <NavLink to="/" className="items-center hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:flex dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-600 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+          3
+      </NavLink>
+
+      <NavLink to="/" className="flex items-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-600 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+          Next
+      </NavLink>
+    </div>
+  )
+}
+
+export default AppPagination
--- src/components/UI/MovieList.tsx
+++ src/components/UI/MovieList.tsx
@@ -1,4 +1,5 @@
 import MovieResponse from "../../types/movieresponse";
+import AppPagination from "./AppPagination";
 import MovieCard from "./MovieCard"
 
 type Props = {
@@ -8,11 +9,14 @@ type Props = {
 const MovieList: React.FC<Props> = ({ response } : Props) => {
   const { results } = response
   return (
-    <div className="py-8 px-8 grid justify-items-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
-      {results.map( movie => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </div>
+    <>
+      <div className="py-8 px-8 grid justify-items-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
+        {results.map( movie => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+      <AppPagination />
+    </>
   )
 }
 
