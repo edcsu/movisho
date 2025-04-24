a3a3ff45e552953d8a2db89ae059cfec78dff658
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-04-24 13:09:56 +0300

    test: add index page test

--- src/pages/__tests__/ErrorPage.test.tsx
+++ src/pages/__tests__/ErrorPage.test.tsx
@@ -1,7 +1,7 @@
 import { describe, it, expect, vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
-import ErrorPage from './ErrorPage'
+import ErrorPage from '../ErrorPage'
 import { useLocation, useRouteError } from 'react-router'
 
 vi.mock('react-router', () => ({
